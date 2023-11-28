1c4bff5c4758db9e6ffed02306c5ded8f546fe86
emsead <150093781+emsead@users.noreply.github.com>
2023-11-28 11:08:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-document.getElementById("download").addEventListener("click", function() {
-    var imgUrl = 'resume.jpg';
+function togglePicture() {
+	var sourceOfPicture = "Resume.jpg";
+	var img = document.getElementById('resume');
   
-    var link = document.createElement('a');
-    link.href = imgUrl;
-    link.download = 'VelascoResume.jpg';
-  
-    document.body.appendChild(link);
-    link.click();
-  
-    document.body.removeChild(link);
-  });
-  
+
+	if (img.style.display === "block") {
+	
+	  img.style.display = "none";
+	} else {
+
+	  img.src = sourceOfPicture.replace('900x900', '1920x1920');
+	  img.style.width = "100%";
+	  img.style.height = "auto";
+	  img.style.display = "block";
+	}
+  }
