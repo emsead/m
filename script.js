770cecaa200983544fbb41c97d1363c904504c91
emsead <150093781+emsead@users.noreply.github.com>
2023-11-28 11:42:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.getElementById("cv").addEventListener("click", function() {
-    var imgUrl = 'Resume.jpg';
+document.getElementById("download").addEventListener("click", function() {
+    var imgUrl = 'resume.jpg';
   
     var link = document.createElement('a');
     link.href = imgUrl;
-    link.download = 'DomingoCV.jpg';
+    link.download = 'ResumeDomings.jpg';
   
     document.body.appendChild(link);
     link.click();
